1c680d171a87fd0a01e872d4fcb3e6aadce57c3c
Simon Norris <snorris@hillcrestgeo.ca>
2022-03-03 17:35:24 -0800

    fix notation -> aquifer matching

--- jobs/sql/wls_water_notation_aquifers_sp.sql
+++ jobs/sql/wls_water_notation_aquifers_sp.sql
@@ -8,11 +8,6 @@ create table if not exists nr_water_notations.wls_water_notation_aquifers_sp
 
 delete from nr_water_notations.wls_water_notation_aquifers_sp;
 
--- not sure how to identify aquifer based notations.
--- for testing:
--- use all PWS notations with no blue_line_key, and find
--- intersecting aquifer, but return only the notation/aquifer pair 
--- of the notation closest to the centre of the aquifer
 
 insert into nr_water_notations.wls_water_notation_aquifers_sp 
 (
@@ -22,15 +17,30 @@ insert into nr_water_notations.wls_water_notation_aquifers_sp
   geom
 )
 
-select distinct on (aquifer_id)
+select 
   a.aquifer_id,
   n.notation_id,
   n.notation_description,
   st_multi(a.geom) as geom
 from nr_water_notations.notations_src n
 inner join whse_water_management.gw_aquifers_classification_svw a
-on st_contains(a.geom, n.geom)
-where 
-  blue_line_key is null and 
-  n.notation_type = 'PWS'
-order by aquifer_id, st_distance(n.geom, st_centroid(a.geom));
+on a.aquifer_id = split_part(n.notation_description, ' - ', 1)::integer
+-- Aquifers are identified by a number and then the dash eg 1010 - 
+-- If there is a mix of numbers and letters before the first dash (-), 
+-- such as this example, it is not an aquifer  117 MILE CREEK - FR - 1986/06/23
+where notation_description ~ '^[0-9]+ -.*$' 
+order by aquifer_id, notation_description;
+
+-- report on unmatched notations 
+-- (and do a spatial join to aquifers in hopes that something matches)
+-- select 
+--   n.notation_id,
+--   n.notation_description,
+--   a2.aquifer_id
+-- from nr_water_notations.notations_src n
+-- left outer join whse_water_management.gw_aquifers_classification_svw a
+-- on a.aquifer_id = split_part(n.notation_description, ' - ', 1)::integer
+-- inner join whse_water_management.gw_aquifers_classification_svw a2
+-- on st_intersects(n.geom, a2.geom)
+-- where notation_description ~ '^[0-9]+ -.*$' and a.aquifer_id is null
+-- order by notation_description;
