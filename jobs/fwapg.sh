bfc3ddec883826fae4d27aecc01ec6282bbdf278
Simon Norris <snorris@hillcrestgeo.ca>
2022-04-08 12:33:39 -0700

    delete gpkg if running the job

--- jobs/fwapg.sh
+++ jobs/fwapg.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Detect if any changes have occured
-# if python detect_source_changes.py https://nrs.objectstore.gov.bc.ca/dzzrch/fwa.gpkg.gz
+# Detect changes in fwa.gpkg.gz on object store and run job if file has changed
+# if python detect_source_changes.py https://nrs.objectstore.gov.bc.ca/dzzrch/fwa.gpkg.gz; then
+
+    # delete existing fwa archive
+    rm -rf data/fwa.gpkg
 
     # clean out the database before re-loading
     psql $DATABASE_URL -c "drop schema if exists whse_basemapping cascade"
