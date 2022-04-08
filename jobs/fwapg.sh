1392494731496705973f841eb542d259cdbfa112
Simon Norris <snorris@hillcrestgeo.ca>
2022-04-08 12:16:21 -0700

    fix syntax

--- jobs/fwapg.sh
+++ jobs/fwapg.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 # if python detect_source_changes.py https://nrs.objectstore.gov.bc.ca/dzzrch/fwa.gpkg.gz
 
     # clean out the database before re-loading
-    psql $DATABASE_URL "drop schema if exists whse_basemapping cascade"
-    psql $DATABASE_URL "drop schema if exists usgs cascade"
-    psql $DATABASE_URL "drop schema if exists hydrosheds cascade"
-    psql $DATABASE_URL "drop schema if exists postgisftw cascade"
+    psql $DATABASE_URL -c "drop schema if exists whse_basemapping cascade"
+    psql $DATABASE_URL -c "drop schema if exists usgs cascade"
+    psql $DATABASE_URL -c "drop schema if exists hydrosheds cascade"
+    psql $DATABASE_URL -c "drop schema if exists postgisftw cascade"
 
     # run the load
     make
