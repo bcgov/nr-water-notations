c5ec9eafbe99ddc6ffdfbfc1bcf622453b8d3d4e
Simon Norris <snorris@hillcrestgeo.ca>
2022-04-08 12:15:14 -0700

    clean out db before running make, outline for running only if changes are present

--- jobs/fwapg.sh
+++ jobs/fwapg.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
-git clone https://github.com/smnorris/fwapg.git
-cd fwapg
-make
+# Detect if any changes have occured
+# if python detect_source_changes.py https://nrs.objectstore.gov.bc.ca/dzzrch/fwa.gpkg.gz
+
+    # clean out the database before re-loading
+    psql $DATABASE_URL "drop schema if exists whse_basemapping cascade"
+    psql $DATABASE_URL "drop schema if exists usgs cascade"
+    psql $DATABASE_URL "drop schema if exists hydrosheds cascade"
+    psql $DATABASE_URL "drop schema if exists postgisftw cascade"
+
+    # run the load
+    make
+
+#fi
