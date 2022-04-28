ba46471e1a8538f99513cc50235c6460cb70565d
Simon Norris <snorris@hillcrestgeo.ca>
2022-04-28 08:32:12 -0700

    fix #17

--- jobs/sql/streams.sql
+++ jobs/sql/streams.sql
@@ -5,34 +5,12 @@ insert into nr_water_notations.streams
   upstream_route_measure
 )
 
-with lowest_notations as
-(
-select distinct
-   a.*
-from nr_water_notations.notations a
-left outer join nr_water_notations.notations b
-on fwa_downstream(
-  a.blue_line_key,
-  a.downstream_route_measure,
-  a.wscode_ltree,
-  a.localcode_ltree,
-  b.blue_line_key,
-  b.downstream_route_measure,
-  b.wscode_ltree,
-  b.localcode_ltree,
-  False,
-  1
-)
-where a.watershed_group_code = :'wsg'
-and b.notation_id is null
-)
-
 select distinct
   b.linear_feature_id,
   b.blue_line_key,
   b.downstream_route_measure,
   b.upstream_route_measure
-from lowest_notations a
+from nr_water_notations.notations a
 inner join whse_basemapping.fwa_stream_networks_sp b
 on fwa_upstream(
   a.blue_line_key,
@@ -53,4 +31,5 @@ or
   a.downstream_route_measure < b.upstream_route_measure and 
   a.downstream_route_measure >= b.downstream_route_measure
 )
+where a.watershed_group_code = :'wsg'
 on conflict do nothing;
