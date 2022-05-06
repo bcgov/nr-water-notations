4682da2026f579c29b25b67640c0d02ab7a6639c
Simon Norris <snorris@hillcrestgeo.ca>
2022-05-05 17:27:19 -0700

    fix #20

--- jobs/sql/streams.sql
+++ jobs/sql/streams.sql
@@ -1,6 +1,7 @@
 insert into nr_water_notations.streams
 ( linear_feature_id,
   blue_line_key,
+  watershed_key,
   downstream_route_measure,
   upstream_route_measure,
   watershed_group_code
@@ -9,6 +10,7 @@ insert into nr_water_notations.streams
 select distinct
   b.linear_feature_id,
   b.blue_line_key,
+  b.watershed_key,
   b.downstream_route_measure,
   b.upstream_route_measure,
   b.watershed_group_code
@@ -32,6 +34,16 @@ or
   a.blue_line_key = b.blue_line_key and 
   a.downstream_route_measure < b.upstream_route_measure and 
   a.downstream_route_measure >= b.downstream_route_measure
+)
+-- also include side channels with equivalent wscodes and null localcodes
+-- (these are impossible to exactly locate without delving into network traces,
+-- but we can include them in this first pass and include only those that
+-- are likely to be upstream of a given notation in subsequent steps)
+or
+(
+  a.wscode_ltree = b.wscode_ltree and
+  b.localcode_ltree is null and
+  b.blue_line_key != b.watershed_key
 )
 where a.watershed_group_code = :'wsg'
 on conflict do nothing;
